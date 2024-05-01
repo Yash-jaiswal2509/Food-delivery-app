7a2ff94870f3cd793f97b0ea225459c4c9bf3389
Yash-jaiswal2509 <yashjaiswal2509@gmail.com>
2024-05-01 22:29:50 +0530

    logo issue

--- frontend/src/assets/frontend_assets/assets.js
+++ frontend/src/assets/frontend_assets/assets.js
@@ -1,7 +1,7 @@
 import basket_icon from "./basket_icon.png";
-import logo from "./Logo.png";
-import footerLogo from "./footerLogo.png"
-import thumbnail from "./Khana on wheels(thumbnail).png"
+import logo from "./logo.png";
+import footerLogo from "./footerLogo.png";
+import thumbnail from "./Khana on wheels(thumbnail).png";
 import header_img from "./header_img.png";
 import search_icon from "./search_icon.png";
 import menu_1 from "./menu_1.png";
@@ -87,4 +87,3 @@ export const menu_list = [
     menu_image: menu_8,
   },
 ];
-
