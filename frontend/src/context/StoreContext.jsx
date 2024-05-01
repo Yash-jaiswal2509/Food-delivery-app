1d2baf7e51879686c2de9b63c263816c176623c6
Yash-jaiswal2509 <yashjaiswal2509@gmail.com>
2024-05-01 13:10:08 +0530

    minor changes for deployment

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -6,7 +6,7 @@ export const StoreContext = createContext(null)
 
 const StoreContextProvider = ({ children }) => {
 
-    const url = "http://localhost:4000"
+    const url = import.meta.env.VITE_BACKEND_URL || "http://localhost:4000";
     const [cartItems, setCartItems] = useState({})
     const [token, setToken] = useState("")
     const [food_list, setFoodList] = useState([]);
